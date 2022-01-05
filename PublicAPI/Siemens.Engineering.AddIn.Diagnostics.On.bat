4b784ac42f803e73b7010f4b41c39ceee76aaedd
Mike Hothing <barbos@inbox.ru>
2022-01-05 09:53:26 +0100

    the project structure and data cleaning

--- PublicAPI/Siemens.Engineering.AddIn.Diagnostics.On.bat
+++
@@ -1,7 +0,0 @@
-cd /d %~dp0
-
-@del "..\bin\PublicAPI\AddIn\Loader\Siemens.Engineering.AddIn.Loader.x32.exe.adr"
-@del "..\bin\PublicAPI\AddIn\Loader\Siemens.Engineering.AddIn.Loader.x64.exe.adr"
-
-@copy "..\bin\PublicAPI\AddIn\Loader\Siemens.Engineering.AddIn.Loader.x32.exe.sc_adr" "..\bin\PublicAPI\AddIn\Loader\Siemens.Engineering.AddIn.Loader.x32.exe.adr"
-@copy "..\bin\PublicAPI\AddIn\Loader\Siemens.Engineering.AddIn.Loader.x64.exe.sc_adr" "..\bin\PublicAPI\AddIn\Loader\Siemens.Engineering.AddIn.Loader.x64.exe.adr"
--- data/export-blocks/xpath-exp1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-XPATH="/Document/SW.Blocks.FC/AttributeList/Interface/*[local-name()='Sections' and namespace-uri()='http://www.siemens.com/automation/Openness/SW/Interface/v3']"
-echo "XPath: $XPATH"
-echo "Result: "
-xmlstarlet sel -t -c "$XPATH" $@
